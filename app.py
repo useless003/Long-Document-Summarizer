7d7e1b23124b436a0b294a8f6f1cd4a8200f1749
useless003 <147938264+useless003@users.noreply.github.com>
2023-11-01 13:43:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.probability import FreqDist
 
+nltk.download("punkt")
 
 def get_pdf_text(pdf_docs):
     text = ""
